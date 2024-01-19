51b7db98c22e023d21b4d252bc1eb7c9c264c1af
Jehyann <cgorsse@gaming.tech>
2024-01-19 13:28:05 +0100

    ex 1, case 1 à 3

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,12 +1,35 @@
 #include "Solution1.h"
+#include <cmath>
 
-// Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_1
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+
+    //case 1 et 2
+    if (_from == TemperatureUnits::KELVIN){
+
+        if (_to == TemperatureUnits::CELSIUS) {
+            return _value - 273.0f;
+        }
+
+        else if (_to == TemperatureUnits::FAHRENHEIT) {
+            return (_value - 273.0f) * 9.0f / 5.0f + 32.0f;
+        }
+
+    }
+
+    //case 3
+    if (_from == _to)
+    {
+        return _value;
+    }
+
+
+
+
+	//return -1.0f;
 }
 
 #endif
